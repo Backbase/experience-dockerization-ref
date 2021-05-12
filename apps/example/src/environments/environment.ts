f833515b75781e98f32a90fddc6fb3cbe5c55b1a
Serhii Koshman <koshman.animys@gmail.com>
2021-05-12 16:24:08 +0300

    add explanation comment for the use of pageModel for local development

--- apps/example/src/environments/environment.ts
+++ apps/example/src/environments/environment.ts
@@ -11,6 +11,10 @@ export const environment = {
   production: false,
   mockProviders: [createMocksInterceptor()],
   bootstrap: {
+    // 1. Type casting is needed to bring some clear structure to the imported mock file.
+    // 2. The pageModel is wrapped into a manageable area container in the provisioning package.
+    //    It is used only in the application/experience managers to bring some manageability rules.
+    //    It is completely redundant in the local development.
     pageModel: (pageModelMock as Container).children[0],
     services,
   },
